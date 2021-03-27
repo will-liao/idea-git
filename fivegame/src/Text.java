cc9c2ed8fdaff87c98821d56a2cbcbff9901a1f4
will <2818927810@qq.com>
2021-03-27 09:15:26 +0800

    修改本地工程

--- fivegame/src/Text.java
+++ fivegame/src/Text.java
@@ -2,4 +2,9 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("hellow");
     }
+    public void text(){
+        for (int i=0;i<4;i++){
+            System.out.println(i);
+        }
+    }
 }
